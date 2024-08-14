74e4fd9812618112bf308e08024c5286d260fdac
lipiec-kacper <lipiec2003kacper@gmail.com>
2024-08-14 02:01:31 +0200

    double linked list de ses mort

--- LinkedList/doubleLinkedList.c
+++ LinkedList/doubleLinkedList.c
@@ -15,14 +15,62 @@ int print_list(struct node *nd){
     printf("\n");
     head = head->next; 
   }
+  
+  return 0;
+}
+
+int insert_new_node(int data, struct node *node){
+  struct node *current = node;
+  
+  while (current != NULL){
+    current->prev = current;
+    current = current->next;
+  }
+  current = malloc(sizeof(current));
+  if(current == NULL){
+    printf("Memory allocation failled");
+    return -1;
+  }
+  current->prev = current;
+  current->data = data;
+  current->next = NULL;
+
+  return 0;
+}
+
+int delete_node(int data, struct node *node){
+  struct node *current = node;
+
+  if(current == NULL){
+    printf("The node is empty \n");
+    return -1;
+  }
+
+  while(current->data != data){
+    current = current->next;
+  }
+
+  if(current->prev == NULL){
+    current = current->next;
+    current->prev = NULL;
+  }
+
+  current = current->prev;
+  current->next = current->next->next;
   return 0;
 }
 
 int main(){
   struct node *head = malloc(sizeof(head));
   head->prev = NULL;
-  head->data = 10;
   head->next = NULL;
+  
+  insert_new_node(10, head);
+  insert_new_node(20, head);
+  insert_new_node(30, head);
+  insert_new_node(40, head);
+  insert_new_node(50, head);
+  delete_node(10, head);
   print_list(head);
   return 0;
 }
